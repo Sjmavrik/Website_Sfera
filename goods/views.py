56b11de6d9f98138dc62b921255967be8d3f19b3
Artem_home <sjmavrik@gmail.com>
2024-11-18 20:51:08 +0300

    добавил обработку исключения 404

--- goods/views.py
+++ goods/views.py
@@ -1,32 +1,30 @@
 from django.core.paginator import Paginator
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_list_or_404, render
 
 from goods.models import Category, Products
 
 
 def catalog(request, category_slug):
 
-    if category_slug == 'all':
-        goods = Products.objects.all()        
+    if category_slug == "all":
+        goods = Products.objects.all()
     else:
-        goods = Products.objects.filter(category__slug=category_slug)
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
     paginator = Paginator(goods, 3)
     current_page = paginator.page(1)
 
     context = {
-        'title': 'Каталог',
-        'goods': current_page,        
-    }    
-    return render(request, 'goods/catalog.html', context)
+        "title": "Каталог",
+        "goods": current_page,
+    }
+    return render(request, "goods/catalog.html", context)
 
 
 def product(request, product_slug):
 
     product = Products.objects.get(slug=product_slug)
 
-    context = {
-        'product': product
-    }
+    context = {"product": product}
 
-    return render(request, 'goods/product.html', context=context)
+    return render(request, "goods/product.html", context=context)
