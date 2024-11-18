422c4ec7d6dda5de2d895035769a566e1474378f
Artem_home <sjmavrik@gmail.com>
2024-11-18 23:33:49 +0300

    первая пагинация

--- goods/views.py
+++ goods/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_list_or_404, render
 from goods.models import Category, Products
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug, page=1):
 
     if category_slug == "all":
         goods = Products.objects.all()
@@ -12,11 +12,12 @@ def catalog(request, category_slug):
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
     paginator = Paginator(goods, 3)
-    current_page = paginator.page(1)
+    current_page = paginator.page(page)
 
     context = {
         "title": "Каталог",
         "goods": current_page,
+        "slug_url": category_slug,
     }
     return render(request, "goods/catalog.html", context)
 
